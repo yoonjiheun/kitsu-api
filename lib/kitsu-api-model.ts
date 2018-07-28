1f60a497add58a214e87580fe0e32834937604dc
John <yoonjiheun28@gmail.com>
2018-07-28 02:20:48 -0400

    Changed to npm module

---
+++ lib/kitsu-api-model.ts
@@ -0,0 +1,9 @@
+/**
+* Author: Ji Heun Yoon (John)
+* Interface created to show expected response object from Kitsu JSON API
+*/
+interface KitsuApiModel {
+  data: object[];
+}
+
+export = KitsuApiModel;
